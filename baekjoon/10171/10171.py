8835841eb7936e51519ab5b84ef35c2fdd041e6d
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 22:05:58 +0900

    Create 10171.py

---
+++ baekjoon/10171/10171.py
@@ -0,0 +1,8 @@
+'''
+고양이 python 풀이
+https://doctcoder.tistory.com/entry/%EA%B3%A0%EC%96%91%EC%9D%B4-%ED%92%80%EC%9D%B4
+'''
+print("\\    /\\")
+print(" )  ( \')")
+print("(  /  )")
+print(" \\(__)|")
