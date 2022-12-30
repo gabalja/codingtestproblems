b3c83a7f64c70e13a7ecc3c31aa7e270284aa0b0
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 21:27:58 +0900

    Create 18108.py

---
+++ baekjoon/18108/18108.py
@@ -0,0 +1,6 @@
+'''
+1998년생인 내가 태국에서는 2541년생?! python 풀이
+https://doctcoder.tistory.com/entry/1998%EB%85%84%EC%83%9D%EC%9D%B8-%EB%82%B4%EA%B0%80-%ED%83%9C%EA%B5%AD%EC%97%90%EC%84%9C%EB%8A%94-2541%EB%85%84%EC%83%9D-%ED%92%80%EC%9D%B4
+'''
+year=int(input())
+print(year-543)
