f02ba9d3b4f751df8a245a7de312e9a5f028d1d2
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:35:07 +0900

    Create 11654.cpp

---
+++ baekjoon/11654/11654.cpp
@@ -0,0 +1,13 @@
+/*
+아스키 코드 C++ 풀이
+https://doctcoder.tistory.com/entry/%EC%95%84%EC%8A%A4%ED%82%A4-%EC%BD%94%EB%93%9C-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	char c;
+	cin>>c;
+	cout<<(int)c; // 문자 입력을 정수로 바꿈
+}
