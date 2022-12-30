f990b984310187018ee1b81992d1657d7ce74048
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 21:24:00 +0900

    Create 10869.py

---
+++ baekjoon/10869/10869.py
@@ -0,0 +1,10 @@
+'''
+사칙연산 python 풀이
+https://doctcoder.tistory.com/entry/%EC%82%AC%EC%B9%99%EC%97%B0%EC%82%B0-%ED%92%80%EC%9D%B4
+'''
+a,b=map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
