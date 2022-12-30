fe6a3c2c7652f22e3cafd65f3a20c05fd228b257
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:10:12 +0900

    Create 10171.cpp

---
+++ baekjoon/10171/10171.cpp
@@ -0,0 +1,13 @@
+/*
+고양이 C++ 풀이
+https://doctcoder.tistory.com/entry/%EA%B3%A0%EC%96%91%EC%9D%B4-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout<<"\\    /\\" <<'\n';
+	cout<<" )  ( ')" <<'\n';
+	cout<<"(  /  )" <<'\n';
+	cout<<" \\(__)|";
+}
