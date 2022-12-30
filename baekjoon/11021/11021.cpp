3e6eb3303aa5a987832a5743f7494bc96f00279b
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:22:10 +0900

    Create 11021.cpp

---
+++ baekjoon/11021/11021.cpp
@@ -0,0 +1,17 @@
+/*
+A+B - 7 C++ 풀이
+https://doctcoder.tistory.com/entry/AB-7-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int t,a,b;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		cin>>a>>b;
+		cout<<"Case #"<<i+1<<": "<<a+b<<"\n";
+	}
+}
