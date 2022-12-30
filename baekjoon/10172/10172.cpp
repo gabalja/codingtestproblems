7811ce30ec5669e3bcb535de2e21d7c6c3454cee
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:10:50 +0900

    Create 10172.cpp

---
+++ baekjoon/10172/10172.cpp
@@ -0,0 +1,14 @@
+/*
+개 C++ 풀이
+https://doctcoder.tistory.com/entry/%EA%B0%9C-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout<<"|\\_/|" <<'\n';
+	cout<<"|q p|   /}" <<'\n';
+	cout<<"( 0 )\"\"\"\\" <<'\n';
+	cout<<"|\"\^\"\`    |" <<'\n';
+	cout<<"||_/=\\\\__|";
+}
