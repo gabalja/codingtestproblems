8bcc311746b6dd726ca1e1e637847bc9be879bef
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:04:50 +0900

    Create 181108.cpp

---
+++ baekjoon/18108/181108.cpp
@@ -0,0 +1,12 @@
+/*
+A+B C++ 풀이
+https://doctcoder.tistory.com/entry/1998%EB%85%84%EC%83%9D%EC%9D%B8-%EB%82%B4%EA%B0%80-%ED%83%9C%EA%B5%AD%EC%97%90%EC%84%9C%EB%8A%94-2541%EB%85%84%EC%83%9D-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	int year;
+	cin>>year;
+	cout<<year-543;
+}
