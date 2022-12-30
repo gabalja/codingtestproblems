bdeae8bcdae2c091343f57da57cc59ed58cfbfa6
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 13:56:18 +0900

    Create 1001.cpp

---
+++ baekjoon/1001/1001.cpp
@@ -0,0 +1,12 @@
+/*
+A-B C++ 풀이
+https://www.acmicpc.net/problem/1001
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	int first, second;
+	cin >> first >> second;
+  cout << first-second;
+}
