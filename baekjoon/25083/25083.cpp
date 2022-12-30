82501460b7ca0677feb0b62a29c5a1771deedc1a
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:11:30 +0900

    Create 25083.cpp

---
+++ baekjoon/25083/25083.cpp
@@ -0,0 +1,15 @@
+/*
+새싹 C++ 풀이
+https://doctcoder.tistory.com/entry/%EC%83%88%EC%8B%B9-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout<<"         ,r'\"7" <<'\n';
+	cout<<"r`-_   ,\'  ,/" <<'\n';
+	cout<<" \\. \". L_r\'" <<'\n';
+	cout<<"   `~\\/" <<'\n';
+	cout<<"      |" <<'\n';
+	cout<<"      |";
+}
