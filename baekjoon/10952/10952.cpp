cf43833d7cf75d65631586e240b5ca356cd579cd
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:25:05 +0900

    Create 10952.cpp

---
+++ baekjoon/10952/10952.cpp
@@ -0,0 +1,17 @@
+/*
+A+B - 5 C++ 풀이
+https://doctcoder.tistory.com/entry/AB-5-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a,b;
+	while(1)
+	{
+		cin>>a>>b;
+		if(a==0&&b==0) break;
+		cout<<a+b<<"\n";
+	}
+}
