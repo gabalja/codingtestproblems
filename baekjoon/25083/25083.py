53d68ce700e990cfe6fb8f93c704305c92082463
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 23:04:11 +0900

    Create 25083.py

---
+++ baekjoon/25083/25083.py
@@ -0,0 +1,10 @@
+'''
+새싹 python 풀이
+https://doctcoder.tistory.com/entry/%EC%83%88%EC%8B%B9-%ED%92%80%EC%9D%B4
+'''
+print("         ,r'\"7")
+print("r`-_   ,\'  ,/")
+print(" \\. \". L_r\'")
+print("   `~\\/")
+print("      |")
+print("      |")
