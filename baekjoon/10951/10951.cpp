2fc716c66f8b64991aca34ba9878e269fb06b0eb
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:25:40 +0900

    Create 10951.cpp

---
+++ baekjoon/10951/10951.cpp
@@ -0,0 +1,15 @@
+/*
+A+B - 4 C++ 풀이
+https://doctcoder.tistory.com/entry/AB-4-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a,b;
+	while(cin >> a>>b) // 입력이 들어오는 한 동작
+	{
+		cout<<a+b<<"\n";
+	}
+}
