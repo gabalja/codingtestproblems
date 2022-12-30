0999e54e24ea49e361ae99167de580a1ff7852c6
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:07:09 +0900

    Update 3003.cpp

--- baekjoon/3003/3003.cpp
+++ baekjoon/3003/3003.cpp
@@ -1,5 +1,5 @@
 /*
-A+B C++ 풀이
+킹, 퀸, 룩, 비숍, 나이트, 폰 C++ 풀이
 https://doctcoder.tistory.com/entry/%ED%82%B9-%ED%80%B8-%EB%A3%A9-%EB%B9%84%EC%88%8D-%EB%82%98%EC%9D%B4%ED%8A%B8-%ED%8F%B0-%ED%92%80%EC%9D%B4
 */
 #include <iostream>
