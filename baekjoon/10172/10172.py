fe916122d81cbb6794572778104ae245e975b902
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 22:27:19 +0900

    Create 10172.py

---
+++ baekjoon/10172/10172.py
@@ -0,0 +1,9 @@
+'''
+개 python 풀이
+https://doctcoder.tistory.com/entry/%EA%B0%9C-%ED%92%80%EC%9D%B4
+'''
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
