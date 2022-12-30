744b1d886755533053bde23772f37fd0754a6257
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 23:14:54 +0900

    Create 1330.py

---
+++ baekjoon/1330/1330.py
@@ -0,0 +1,8 @@
+'''
+두 수 비교하기 python 풀이
+https://doctcoder.tistory.com/entry/%EB%91%90-%EC%88%98-%EB%B9%84%EA%B5%90%ED%95%98%EA%B8%B0-%ED%92%80%EC%9D%B4
+'''
+a,b=map(int,input().split())
+if(a>b): print('>')
+elif(a<b): print('<')
+else: print("==")
