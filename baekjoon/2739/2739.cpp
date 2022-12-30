5a239a5f65578b32bd513a2eba8f29707a078c36
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:18:43 +0900

    Create 2739.cpp

---
+++ baekjoon/2739/2739.cpp
@@ -0,0 +1,16 @@
+/*
+구구단 C++ 풀이
+https://doctcoder.tistory.com/entry/%EA%B5%AC%EA%B5%AC%EB%8B%A8-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<10;i++)
+	{
+		cout<<n<<" * "<<i<<" = "<<n*i<<"\n";		
+	}
+}
