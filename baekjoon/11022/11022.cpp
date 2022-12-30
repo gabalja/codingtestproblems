025109eb7f1b7e05dcf64e5d52ae2872bc0bed2f
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:23:01 +0900

    Create 11022.cpp

---
+++ baekjoon/11022/11022.cpp
@@ -0,0 +1,17 @@
+/*
+A+B - 8 C++ 풀이
+https://doctcoder.tistory.com/entry/AB-8-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int t,a,b;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		cin>>a>>b;
+		cout<<"Case #"<<i+1<<": "<<a<<" + "<<b<<" = "<<a+b<<"\n";
+	}
+}
