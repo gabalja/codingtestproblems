3414cee278d4dc6e3905b98cc250e3d6aa691903
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 14:00:51 +0900

    Create 1008.cpp

---
+++ baekjoon/1008/1008.cpp
@@ -0,0 +1,13 @@
+/*
+A/B C++ 풀이
+https://www.acmicpc.net/problem/1008
+*/
+#include <iostream>
+
+using namespace std;
+int main() {
+	double first, second;
+	cin >> first >> second;
+	cout.precision(10); // 소수점 
+  cout << first/second;
+}
