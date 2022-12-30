81f2f0aeca671a40efab8a4de50cd201eefd005d
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 23:18:11 +0900

    Create 9498.py

---
+++ baekjoon/9498/9498.py
@@ -0,0 +1,10 @@
+'''
+시험 성적 python 풀이
+https://doctcoder.tistory.com/entry/%EC%8B%9C%ED%97%98-%EC%84%B1%EC%A0%81-%ED%92%80%EC%9D%B4
+'''
+score=int(input())
+if(score>89): print('A')
+elif(score>79): print('B')
+elif(score>69): print('C')
+elif(score>59): print('D')
+else: print('F')
