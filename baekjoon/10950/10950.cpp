d3e50421a35becd2462c474c4e67ecbb6a7d42b1
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-30 18:19:23 +0900

    Create 10950.cpp

---
+++ baekjoon/10950/10950.cpp
@@ -0,0 +1,18 @@
+/*
+A+B - 3 C++ 풀이
+https://doctcoder.tistory.com/entry/AB-3-%ED%92%80%EC%9D%B4
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		cout<<a+b<<"\n";
+	}
+}
