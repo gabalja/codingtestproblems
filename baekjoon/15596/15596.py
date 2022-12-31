a04e70c87d029fccf3884a9621fc667acc6b8066
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-31 18:18:50 +0900

    Create 15596.py

---
+++ baekjoon/15596/15596.py
@@ -0,0 +1,9 @@
+'''
+정수 N개의 합 python 풀이
+https://doctcoder.tistory.com/entry/%EC%A0%95%EC%88%98-N%EA%B0%9C%EC%9D%98-%ED%95%A9-%ED%92%80%EC%9D%B4
+'''
+def solve(a):
+    ans = 0
+    for i in a:
+        ans+=i
+    return ans
