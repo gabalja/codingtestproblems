0879bb1a71961a5afb911e31c751dd72427c7423
gabalja <80046476+gabalja@users.noreply.github.com>
2022-12-31 10:23:03 +0900

    Create 11021.py

---
+++ baekjoon/11021/11021.py
@@ -0,0 +1,8 @@
+'''
+A+B - 7 python 풀이
+https://doctcoder.tistory.com/entry/AB-7-%ED%92%80%EC%9D%B4
+'''
+t=int(input())
+for i in range(t):
+	a,b= map(int,input().split())
+	print(f"Case #{i+1}: {a+b}")
