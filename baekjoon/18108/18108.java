b3eb5a37fcd2f974f250fc10c62acf723560f2a4
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 00:55:15 +0900

    Create 18108.java

---
+++ baekjoon/18108/18108.java
@@ -0,0 +1,15 @@
+/*
+A+B Java 풀이
+https://doctcoder.tistory.com/entry/AB-%ED%92%80%EC%9D%B4
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+	{
+    Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a-543);
+  }
+}
