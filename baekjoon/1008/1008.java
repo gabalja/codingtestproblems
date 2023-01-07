fd4efff8ea829ce02965d0be28d32daaa83039d3
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 00:45:04 +0900

    Create 1008.java

---
+++ baekjoon/1008/1008.java
@@ -0,0 +1,16 @@
+/*
+A/B Java 풀이
+https://doctcoder.tistory.com/entry/AB-%ED%92%80%EC%9D%B4-1
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+	{
+    Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		System.out.printf("%.9f",a/b);
+  }
+}
