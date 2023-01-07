c17d8be4a8914731cb8b450916e16d2bf96785d9
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 00:38:21 +0900

    Create 1000.java

---
+++ baekjoon/1000/1000.java
@@ -0,0 +1,16 @@
+/*
+A+B Java 풀이
+https://doctcoder.tistory.com/entry/AB-%ED%92%80%EC%9D%B4
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+	{
+    Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+  }
+}
