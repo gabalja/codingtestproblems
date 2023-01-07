d7902b977fc72fba61e4871cbe5cbc0a72bedf1a
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 00:53:08 +0900

    Create 10926.java

---
+++ baekjoon/10926/10926.java
@@ -0,0 +1,15 @@
+/*
+??! Java 풀이
+https://doctcoder.tistory.com/entry/%ED%92%80%EC%9D%B4
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+	{
+    Scanner sc = new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(s+"??!");
+  }
+}
