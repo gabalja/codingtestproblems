09dcf47ac3e9c80cf23dfe1aaebf17498bf7a470
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 00:37:16 +0900

    Create 2557.java

---
+++ baekjoon/2557/2557.java
@@ -0,0 +1,9 @@
+/*
+Hello, World Java 풀이
+https://doctcoder.tistory.com/entry/Hello-World-%ED%92%80%EC%9D%B4
+*/
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
