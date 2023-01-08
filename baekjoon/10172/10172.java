d2f3cf09ae3474de8f0a3e435bb2c92893fdde9f
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 10:49:25 +0900

    Create 10172.java

---
+++ baekjoon/10172/10172.java
@@ -0,0 +1,17 @@
+/*
+개 Java 풀이
+https://doctcoder.tistory.com/entry/%EA%B0%9C-%ED%92%80%EC%9D%B4
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args)
+  {
+      System.out.println("|\\_/|");
+      System.out.println("|q p|   /}");
+      System.out.println("( 0 )\"\"\"\\");
+      System.out.println("|\"^\"`    |");
+			System.out.println("||_/=\\\\__|");
+  }
+}
