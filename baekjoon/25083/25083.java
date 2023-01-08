600d28d9bc329a68148d64f92063ea701f48a5ef
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-08 10:53:00 +0900

    Create 25083.java

---
+++ baekjoon/25083/25083.java
@@ -0,0 +1,18 @@
+/*
+새싹 Java 풀이
+https://doctcoder.tistory.com/entry/%EC%83%88%EC%8B%B9-%ED%92%80%EC%9D%B4
+*/
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args)
+  {
+      System.out.println("         ,r'\"7");
+      System.out.println("r`-_   ,\'  ,/");
+      System.out.println(" \\. \". L_r\'");
+      System.out.println("   `~\\/");
+			System.out.println("      |");
+			System.out.println("      |");
+  }
+}
