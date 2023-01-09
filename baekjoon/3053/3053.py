05ebdce74ec3168262bbb19b7bab27c154b1935c
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-09 10:19:49 +0900

    Create 3053.py

---
+++ baekjoon/3053/3053.py
@@ -0,0 +1,10 @@
+'''
+택시 기하학 python 풀이
+https://doctcoder.tistory.com/entry/%ED%83%9D%EC%8B%9C-%EA%B8%B0%ED%95%98%ED%95%99-%ED%92%80%EC%9D%B4
+'''
+pi=3.1415926535897;
+r=float(input())
+eure=pi*r*r
+taxi=r*r*2
+print(eure)
+print(taxi)
