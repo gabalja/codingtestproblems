bfb4928463e6a7982c9960154af01564a1a365a2
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-16 15:39:27 +0900

    Update 17103.py

--- baekjoon/17103/17103.py
+++ baekjoon/17103/17103.py
@@ -2,27 +2,29 @@
 골드바흐 파티션 python 풀이
 https://doctcoder.tistory.com/entry/%EA%B3%A8%EB%93%9C%EB%B0%94%ED%9D%90-%ED%8C%8C%ED%8B%B0%EC%85%98-%ED%92%80%EC%9D%B4
 '''
-import sys, math
+def get_primary_list(n):
+    array = [1 for _ in range(n+1)]
 
-input = sys.stdin.readline
+    for i in range(2, int(n ** 0.5) + 1):
+        if array[i]:
+            j = 2
 
-n = 1000000
-array = [True for i in range(n + 1)]
-prime = []
-for i in range(2, n + 1):
-    if array[i] == True:
-        j = 2
-        prime.append(i)
         while i * j <= n:
-            array[i * j] = False
+            array[i * j] = 0
             j += 1
 
-for _ in range(int(input())):
-    n = int(input())
-    cnt = 0
-    for i in range(n // 2 , 1 , -1):
-        if array[i] == True and array[n-i] == True:
-            cnt += 1
-        else:
-            continue
-    print(cnt)
+    return array
+
+T = int(input())
+nums = [int(input()) for _ in range(T)]
+max_num = max(nums)
+primarys = get_primary_list(max_num)
+
+for num in nums:
+    result = 0
+
+    for i in range(2, num // 2 + 1):
+        if primarys[i] and primarys[num - i]:
+            result += 1
+
+    print(result)
