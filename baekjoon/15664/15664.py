ce7791da27b368f0ecd7cd841be0e80f9979cbc8
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-24 16:54:33 +0900

    Create 15664.py

---
+++ baekjoon/15664/15664.py
@@ -0,0 +1,15 @@
+'''
+N과 M (10) python 풀이
+https://doctcoder.tistory.com/entry/N%EA%B3%BC-M-10-%ED%92%80%EC%9D%B4
+'''
+from itertools import combinations
+
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort()
+res=list(set(combinations(arr,m)))
+res.sort()
+for i in res:
+	for j in i:
+		print(j,end=' ')
+	print()
