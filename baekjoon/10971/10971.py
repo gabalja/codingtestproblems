5dd01183cc5dfd2414438c09a79dbe4e19c3aa28
gabalja <80046476+gabalja@users.noreply.github.com>
2023-01-25 13:12:00 +0900

    Update 10971.py

--- baekjoon/10971/10971.py
+++ baekjoon/10971/10971.py
@@ -1,21 +1,4 @@
 '''
-차이를 최대로 python 풀이
-https://doctcoder.tistory.com/entry/%EC%B0%A8%EC%9D%B4%EB%A5%BC-%EC%B5%9C%EB%8C%80%EB%A1%9C-%ED%92%80%EC%9D%B4
-'''
-from itertools import permutations
-
-n=int(input())
-inp=list(map(int,input().split()))
-per=list(permutations(inp,n))
-res=-10000000
-for i in per:
-	temp=0
-	for j in range(n-1):
-		temp+=abs(i[j]-i[j+1])
-	if temp>res: res=temp
-print(res)
-
-'''
 외판원 순회 2 python 풀이
 https://doctcoder.tistory.com/entry/%EC%99%B8%ED%8C%90%EC%9B%90-%EC%88%9C%ED%9A%8C-2-%ED%92%80%EC%9D%B4
 '''
