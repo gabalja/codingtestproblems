1171ad55f759be111716c326d92a0936c46776c0
gabalja <80046476+gabalja@users.noreply.github.com>
2023-02-09 00:26:54 +0900

    Create 13549.py

---
+++ baekjoon/13549/13549.py
@@ -0,0 +1,26 @@
+'''
+숨바꼭질 3 python 풀이
+https://doctcoder.tistory.com/entry/%EC%88%A8%EB%B0%94%EA%BC%AD%EC%A7%88-3-%ED%92%80%EC%9D%B4
+'''
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque()
+q.append(n) 
+visited = [-1 for _ in range(100001)]
+visited[n] = 0
+
+while q:
+    s = q.popleft()
+    if s == k:
+        print(visited[s])
+        break
+    if 0 <= s-1 < 100001 and visited[s-1] == -1:
+        visited[s-1] = visited[s] + 1
+        q.append(s-1)
+    if 0 < s*2 < 100001 and visited[s*2] == -1:
+        visited[s*2] = visited[s]
+        q.appendleft(s*2)
+    if 0 <= s+1 < 100001 and visited[s+1] == -1:
+        visited[s+1] = visited[s] + 1
+        q.append(s+1)
